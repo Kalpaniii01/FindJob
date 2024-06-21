d69014bf227455a9f5a6f855fdbe081ece801d40
KBCHANUKAJ <kavindhyaak.21@uom.lk>
2024-06-21 23:14:13 +0530

    Create  navbar and footer

---
+++ templates/footer.php
@@ -0,0 +1,32 @@
+<footer class="bg-light text-center text-lg-start">
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Contact Us</h5>
+                <p>
+                    You can reach us at: <br>
+                    Email: contact@yoursite.com <br>
+                    Phone: +94 11 234 5678
+                </p>
+            </div>
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Follow Us</h5>
+                <a href="#" class="me-4 text-reset">
+                    <i class="bi bi-facebook"></i>
+                </a>
+                <a href="#" class="me-4 text-reset">
+                    <i class="bi bi-twitter"></i>
+                </a>
+                <a href="#" class="me-4 text-reset">
+                    <i class="bi bi-instagram"></i>
+                </a>
+                <a href="#" class="me-4 text-reset">
+                    <i class="bi bi-linkedin"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2023 YourSite. Developed by [Your Name].
+    </div>
+</footer>
